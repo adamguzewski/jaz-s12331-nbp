d725ae032f54367c2b1d03feca34b334a10fb8b9
Adam <mail2gzs@gmail.com>
2021-07-04 16:37:17 +0200

    initial

---
+++ src/main/java/adaguz/jazs12331nbp/JazS12331NbpApplication.java
@@ -0,0 +1,13 @@
+package adaguz.jazs12331nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS12331NbpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JazS12331NbpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/adaguz/jazs12331nbp/JazS12331NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package adaguz.jazs12331nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS12331NbpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
